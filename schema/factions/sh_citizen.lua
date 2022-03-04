83b0f04e926b2ce30296a61cad903b2c493196ab
Ellis F. Derbyshire <ellisder@hotmail.co.uk>
2022-03-04 12:56:04 +0000

    feat: set up custom citizen models
    * Uses Enhanced Citizens V3 (https://steamcommunity.com/sharedfiles/filedetails/?id=677125227)

--- schema/factions/sh_citizen.lua
+++ schema/factions/sh_citizen.lua
@@ -3,6 +3,16 @@ FACTION.name = "Citizen"
 FACTION.description = "A regular human citizen enslaved by the Universal Union."
 FACTION.color = Color(150, 125, 100, 255)
 FACTION.isDefault = true
+FACTION.models = {"models/player/zelpa/female_06.mdl", "models/player/zelpa/female_07.mdl"}
+
+for i = 1, 4 do
+	table.insert(FACTION.models, "models/player/zelpa/female_0"..i..".mdl")
+end
+
+for i = 1, 11 do
+	table.insert(FACTION.models, "models/player/zelpa/male_0"..i..".mdl")
+end
+
 
 function FACTION:OnCharacterCreated(client, character)
 	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
